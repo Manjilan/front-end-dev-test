e0f00b278f34edb1806848ba0d0397e00ef42297
Manjilan <nakarmimanjila@yahoo.com>
2018-11-02 02:54:43 -0700

    testing deployment

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
-var mongoose = require("mongoose");
-mongoose.Promise = global.Promise;
-
-mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/front-end-dev-test");
-
-module.exports.Message = require("./message");
+// var mongoose = require("mongoose");
+// mongoose.Promise = global.Promise;
+//
+// mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/front-end-dev-test");
+//
+// module.exports.Message = require("./message");
