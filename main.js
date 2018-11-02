ef33309234251ac26cd733faf282fdf896f53eec
Manjilan <nakarmimanjila@yahoo.com>
2018-11-02 02:51:42 -0700

    testing deployment

--- main.js
+++ main.js
@@ -9,35 +9,35 @@ app.use(bodyParser.urlencoded({ extended: true, }));
 app.use(express.static('public'));
 
 // set Models
-var db = require('./models'),
-Message = db.Message;
+// var db = require('./models'),
+// Message = db.Message;
 
 app.get("/", function(req, res){
   res.sendfile('views/index.html');
 })
 //Get Messages from the form
-app.get("/messages", function(req,res){
-    Message.find({}, function(err, allMessages){
-      if (err){
-        console.log(err);
-      } else {
-        res.json(allMessages);
-      }
-    })
-});
+// app.get("/messages", function(req,res){
+//     Message.find({}, function(err, allMessages){
+//       if (err){
+//         console.log(err);
+//       } else {
+//         res.json(allMessages);
+//       }
+//     })
+// });
 
 //Create Message
-app.post("/messages", function(req, res){
-  var newMessage = new Message(req.body);
-  console.log(req.body);
-  newMessage.save(function(err, savedMessage){
-    if (err){
-      console.log(err)
-    } else {
-      res.redirect("/");
-    }
-  })
-});
+// app.post("/messages", function(req, res){
+//   var newMessage = new Message(req.body);
+//   console.log(req.body);
+//   newMessage.save(function(err, savedMessage){
+//     if (err){
+//       console.log(err)
+//     } else {
+//       res.redirect("/");
+//     }
+//   })
+// });
 
 //-------------Server------------->
 app.listen(process.env.PORT || 3000, function(){
